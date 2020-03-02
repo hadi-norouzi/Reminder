536d5d661bc07f0fa38d9eae9b9952af5dc7f792
hadi norouzi <hadinorouzi78@gmail.com>
2020-03-02 16:10:09 +0330

    create toolbar component and add to main activity

---
+++ app/src/main/java/com/hadinorouzi/reminder/MainActivity.java
@@ -0,0 +1,19 @@
+package com.hadinorouzi.reminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+}
