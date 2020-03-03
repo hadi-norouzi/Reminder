a790ba82fbe82fdf68fd542ced42e17ae1f8fd23
hadi norouzi <hadinorouzi78@gmail.com>
2020-03-03 20:43:49 +0330

    add back (up) arrow to addNewReminder activity

--- app/src/main/java/com/hadinorouzi/reminder/AddNewReminder.java
+++ app/src/main/java/com/hadinorouzi/reminder/AddNewReminder.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AddNewReminder extends AppCompatActivity {
 
@@ -11,11 +12,18 @@ public class AddNewReminder extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_reminder);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        toolbar.setTitle("add Reminder");
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("add Reminder");
 
 //        setSupportActionBar(toolbar);
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
     }
 }
