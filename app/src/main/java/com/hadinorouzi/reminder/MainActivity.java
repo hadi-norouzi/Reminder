c91d274e50522a587932046004038abd301c3953
hadi norouzi <hadinorouzi78@gmail.com>
2020-03-03 17:02:49 +0330

    add floatingActionButton to main activity

--- app/src/main/java/com/hadinorouzi/reminder/MainActivity.java
+++ app/src/main/java/com/hadinorouzi/reminder/MainActivity.java
@@ -1,12 +1,19 @@
 package com.hadinorouzi.reminder;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton FAB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +22,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        FAB = findViewById(R.id.floatingAB);
+    }
+
+    public void FABClicked(View view){
+        Intent intent = new Intent(this,AddNewReminder.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void setSupportActionBar(@Nullable Toolbar toolbar) {
+        super.setSupportActionBar(toolbar);
     }
 }
